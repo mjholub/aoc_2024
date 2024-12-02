92250a332bb508f172fbb94fd8448cd9f5b57789
Marcelina Hołub <mholub@tutanota.com>
2024-12-02 12:40:55 +0100

    day1 (pt2): add solution

--- src/aoc_2024/day_one.clj
+++ src/aoc_2024/day_one.clj
@@ -1,21 +1,28 @@
-(ns aoc-2024.day-one 
+(ns aoc-2024.day-one
   (:require
    [aoc-2024.core :refer [get-input-text]]
    [clojure.string :as str]))
 
 (defn clean-pairs [input]
-  (->> input 
+  (->> input
        (mapcat #(str/split (str/trim %) #"\s+"))
        (mapv #(parse-long %))))
 
 (defn make-list [v] (sort (vec (take-nth 2 v))))
+
+(defn produce-second-part-answer [left-list right-list]
+  (let [right-frequencies (frequencies right-list)]
+    (->> left-list
+         (mapv (fn [n]
+                 (let [freq (get right-frequencies n 0)]
+                   (* n freq))))
+         (reduce +))))
 
 (defn answer []
   (let [pairs (str/split (get-input-text "one") #"\n")
         cleaned (clean-pairs pairs)
         left-list (make-list (rest cleaned)) ;; odd indices
         right-list (make-list cleaned)]
-  {:part-one
-   (reduce + (mapv #(abs (- %1 %2)) left-list right-list))
-   :part-two nil
-   }))
+    {:part-one
+     (reduce + (mapv #(abs (- %1 %2)) left-list right-list))
+     :part-two (produce-second-part-answer left-list right-list)}))
